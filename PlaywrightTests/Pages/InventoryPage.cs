068f3db58d03ffadb6ebb9569319e487e642edb0
marilou-ranoy <marilou.ranoy@advantagesolutions.net>
2023-07-12 02:01:51 +0800

    some minor changes

--- PlaywrightTests/Pages/InventoryPage.cs
+++ PlaywrightTests/Pages/InventoryPage.cs
@@ -11,11 +11,9 @@ namespace PlaywrightTests.Pages
     {
         private readonly IPage _page;
         public InventoryPage(IPage page) => _page = page;
-
         private ILocator IconShoppingCart => _page.Locator("#shopping_cart_container a");
         private ILocator BtnContinueShopping => _page.Locator("[data-test=\"continue-shopping\"]");
         private ILocator BtnAddToCart => _page.Locator("[data-test=\"add-to-cart-sauce-labs-backpack\"]");
-
         private ILocator LinkItemInCart => _page.GetByRole(AriaRole.Link, new() { Name = "Sauce Labs Backpack" });
 
         public ILocator GetLinkItemInCart()
